e893824ce0c84b376111f490159d7a81c99a5014
Nikita Kalinin <mail@nvkalinin.dev>
2022-02-23 20:31:02 +0300

    В хранилище Memory добавлены блокировки для конкурентного доступа.

--- store/engine/memory.go
+++ store/engine/memory.go
@@ -2,11 +2,13 @@ package engine
 
 import (
 	"github.com/nvkalinin/business-calendar/store"
+	"sync"
 	"time"
 )
 
 type Memory struct {
 	store map[int]store.Months
+	mu    sync.RWMutex
 }
 
 func NewMemory() *Memory {
@@ -21,6 +23,7 @@ func (m *Memory) FindDay(y int, mon time.Month, d int) (*store.Day, bool) {
 		return nil, false
 	}
 
+	// month — отдельная копия, блокировка не нужна.
 	day, ok := month[d]
 	if !ok {
 		return nil, false
@@ -35,6 +38,7 @@ func (m *Memory) FindMonth(y int, mon time.Month) (store.Days, bool) {
 		return nil, false
 	}
 
+	// year — отдельная копия, блокировка не нужна.
 	month, ok := year[mon]
 	if !ok {
 		return nil, false
@@ -44,6 +48,9 @@ func (m *Memory) FindMonth(y int, mon time.Month) (store.Days, bool) {
 }
 
 func (m *Memory) FindYear(y int) (store.Months, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	year, ok := m.store[y]
 	if !ok {
 		return nil, false
@@ -53,6 +60,9 @@ func (m *Memory) FindYear(y int) (store.Months, bool) {
 }
 
 func (m *Memory) PutYear(y int, data store.Months) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	m.store[y] = data.Copy()
 	return nil
 }
